c8ecc6744d60e9020725e7fd505bc57ec3800ad2
zino <yaohsientseng@YAOHSIENdeMacBook-Air.local>
2024-09-23 11:01:22 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.write("Hello, World!")
+st.write(f"NumPy version: {np.__version__}")
+st.write(f"Pandas version: {pd.__version__}")
